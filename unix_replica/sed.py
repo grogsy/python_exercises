f6579540131fa2c72faaaefff71712e0cee197d8
grogsy <a.gray.cloud@gmail.com>
2018-06-20 22:54:36 -0300

    Add files via upload

--- unix_replica/sed.py
+++ unix_replica/sed.py
@@ -2,16 +2,11 @@ import argparse
 import re
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Stream editor')
-    
-    parser.add_argument('files', type=str, nargs='+')
-
-    parser.add_argument('-s', type=str, help='Use this to search for a literal string to substitute')
-
-    parser.add_argument('-e', type=str, help='Use this to search for a regular expression substitution')
-
-    parser.add_argument('-g', type=str, help='The string to replace')
-
+    parser = argparse.ArgumentParser(description='bare implementation of unix sed')
+    parser.add_argument('file', type=str, nargs='+')
+    parser.add_argument('-s', type=str, help='search string pattern')
+    parser.add_argument('-e', type=str, help='search regex pattern')
+    parser.add_argument('-g', type=str, help='pattern to sub')
     return parser.parse_args()
 
 
@@ -23,24 +18,21 @@ def str_replace(txt, target, sub):
     return '\n'.join(line.replace(target, sub) for line in txt)
 
 
-if __name__=='__main__':
-    args = vars(parse_args())
-
-    files = args['files']
+def main(args):
     target = args['g']
+    with open(args['file'], 'r+') as f:
+        txt = f.readlines()
+        f.seek(0)
+        if args['s']:
+            sub = args['s']
+            txt = str_replace(txt, target, sub)
+        elif args['e']:
+            pattern = re.compile(args['e'])
+            txt = re_replace(txt, pattern, target)
+        f.write(txt)
+    return txt
 
-    if args['s'] and args['e']:
-        exit("Can't substitute using both regex and literal string")
 
-    for file in files:
-        with open(file, 'r+') as cur:
-            txt = cur.readlines()
-            cur.seek(0)
-            if args['s']:
-                sub = args['s']
-                txt = str_replace(txt, target, sub)
-            elif args['e']:
-                pattern = re.compile(args['e'])
-                txt = re_replace(txt, pattern, target)
-            cur.write(txt)
-            
+
+if __name__ == '__main__':
+    main(vars(parse_args()))
