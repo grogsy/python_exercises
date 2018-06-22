d04eccc4bb5e38b92554440aa9b61cb65cadde82
grogsy <a.gray.cloud@gmail.com>
2018-06-22 07:10:11 -0300

    Add files via upload

--- unix_replica/unix_things/pipe.py
+++ unix_replica/unix_things/pipe.py
@@ -3,7 +3,17 @@ requirements:
 
             command piping should be read from left to right
             an example would be something like:
-                -history | sed <file_arg> <opts> | cut <opts> | sort <opts> | uniq
+
+                cat .bash_history | sed <file_arg> <opts> | cut <opts> | sort <opts> | uniq
+
+            and this is what it would look like being run as a python program:
+
+                python pipe.py "cat .bash_history | sed <opts> | cut<opts> | sort <opts> | uniq
+
+            sometimes the leading command does not require a file but returns a text stream instead
+            we will need to handle this case, it could be something like the ls command:
+
+                python pipe.py "ls | sed <opts> | cut <opts> | sort <opts> | uniq
 
             -keeping this in mind, we start by implementing functionality for one type of arg
             and this will be a single text file to process across the pipe
@@ -13,49 +23,52 @@ import subprocess
 import sys
 
 
-# Going to be writing results to a temp file
-# It helps keep track across the different changes we make
-# For every command in the stream we run on the file
 TMP_HANDLE = 'pipe_tmp.txt'
 
-def main(args):
-    command_stream = ' '.join(args['c']).split('|')
 
-    initCmd, *initOpts = command_stream.pop(0).split()
-    txt = subprocess.run(build_cmd(initCmd, initOpts, args['file']), stdout=subprocess.PIPE).stdout
+def main(args):
+    '''main'''
+    command_stream = ' '.join(args['cmds']).split('|')
+    cmd, *opts = command_stream.pop(0).split()
+    full_cmd = ['python', cmd+'.py']
+    if opts:
+        full_cmd += opts
+    txt = subprocess.run(full_cmd, stdout=subprocess.PIPE).stdout
     write_to_temp(txt)
 
     for command in command_stream:
         cmd, *opts = command.strip().split()
-        fullCmd = build_cmd(cmd, opts)
-        txt = subprocess.run(fullCmd, stdout=subprocess.PIPE).stdout
+        full_cmd = build_cmd(cmd, opts)
+        txt = subprocess.run(full_cmd, stdout=subprocess.PIPE).stdout
         write_to_temp(txt)
 
     sys.stdout.write(txt.decode('utf-8'))
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('file', type=str, help='the file to process')
-    parser.add_argument('-c', type=str, nargs='+', help='a command that parses a pipe stream. \
+    '''parse the pipe expression'''
+    desc = 'a program that simulates unix command-line pipe feature'
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('cmds', type=str, nargs='+', help='a command that resembles a pipe stream. \
                         Enclose your pipe command in double-quotes(") otherwise it will not work.')
     return parser.parse_args()
 
 
-def build_cmd(cmd, opts, f=None):
-    '''expected output: python cmd.py file.txt -o opt -foo bar.split()'''
+def build_cmd(cmd, opts):
+    '''This is necessary to parse commands together with the aforementioned temp file'''
     # fullCmd = ['python'] += shlex.split(command)         should try this one out too
-    if not f:
-        f = TMP_HANDLE
-    build = ["python", cmd+".py", f]
+    build = ["python", cmd+".py", TMP_HANDLE]
     if opts:
-        build += [opt for opt in opts.split()]
+        build += opts
     return build
 
 
 def write_to_temp(txt):
-    with open(TMP_HANDLE, 'r+') as tmp:
-        tmp.seek(0)
+    ''' Going to be writing results to a temp file
+    It helps keep track across the different changes we make
+    For every command in the stream we run on the file
+    '''
+    with open(TMP_HANDLE, 'w') as tmp:
         tmp.write(txt.decode('utf-8'))
 
 
