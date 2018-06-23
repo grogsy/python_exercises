1f00b90f6f095608312364ec31bbb3890bff7f5a
grogsy <a.gray.cloud@gmail.com>
2018-06-23 13:47:31 +0800

    Add files via upload

--- unix_replica/unix_things/sed.py
+++ unix_replica/unix_things/sed.py
@@ -6,34 +6,26 @@ import sys
 def parse_args():
     parser = argparse.ArgumentParser(description='bare implementation of unix sed')
     parser.add_argument('file', type=str)
-    parser.add_argument('-s', type=str, help='search string pattern')
-    parser.add_argument('-e', type=str, help='search regex pattern')
-    parser.add_argument('-g', type=str, help='pattern to sub')
+    parser.add_argument('-s', type=str, help='search string or regex to replace')
+    parser.add_argument('-g', type=str, help='replacement expression')
     parser.add_argument('-w', action='store_true', help='write change to current file')
     return parser.parse_args()
 
 
-def re_replace(txt, pattern, target):
-    return '\n'.join(re.sub(pattern, target, line) for line in txt)
-
-
-def str_replace(txt, target, sub):
-    return '\n'.join(line.replace(target, sub) for line in txt)
+def re_replace(txt, new_exp, old_exp):
+    return '\n'.join(old_exp.sub(new_exp, line) for line in txt)
 
 
 def main(args):
-    target = args['g']
+    target = re.compile(args['s'])
+    replacement  = args['g']
+
     with open(args['file'], 'r+') as f:
         txt = f.readlines()
         # clean txt list of any excess/stray new-lines
         txt = [line.strip('\n') for line in txt]
         txt = [line for line in txt if line]
-        if args['s']:
-            sub = args['s']
-            txt = str_replace(txt, target, sub)
-        elif args['e']:
-            pattern = re.compile(args['e'])
-            txt = re_replace(txt, pattern, target)
+        txt = re_replace(txt, replacement, target)
         if args['w']:
             f.seek(0)
             f.write(txt)
