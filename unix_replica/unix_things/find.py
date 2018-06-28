2948f3b7804d3db03a22abbeb69627c9c4c67e51
grogsy <a.gray.cloud@gmail.com>
2018-06-28 13:37:32 +0800

    Add files via upload

--- unix_replica/unix_things/find.py
+++ unix_replica/unix_things/find.py
@@ -16,22 +16,34 @@ def parse_args():
     parser.add_argument('-print', action='store_true', help='display results')
     parser.add_argument('-empty', action='store_true', help='get matches that are either empty or a directory')
     parser.add_argument('-readable', action='store_true', help='get matches that are readable by the current user')
+    parser.add_argument('-executable', action='store_true', help='same as -readable but checks for x-permision')
+    parser.add_argument('-writable', action='store_true', help='same as -readable but checks for w-permission')
     parser.add_argument('-samefile', type=str, help='get matches which have the same inode as <name>')
+    parser.add_argument('-size', type=int, help='get matches that have n size')
 
     return parser.parse_args()
 
 
 def main(args):
     directory = args['cd'] or '.'
-
     os.chdir(directory)
 
     res = [link for link in glob.iglob(args['name'])]
     if args['L']:
         res = [os.path.realpath(link) for link in res]
 
+    # Check permissions
     if args['readable']:
         res = [link for link in res if os.access(link, os.R_OK)]
+    if args['writable']:
+        res = [link for link in res if os.access(link, os.W_OK)]
+    if args['executable']:
+        res = [link for link in res if os.access(link, os.X_OK)]
+
+    # Check other stats
+    if args['size']:
+        res = [link for link in res if os.stat(link).st_size == args['size']]
+
     if args['samefile']:
         res = [link for link in res if os.stat(link).st_ino == os.stat(args['samefile']).st_ino]
     if args['empty']:
