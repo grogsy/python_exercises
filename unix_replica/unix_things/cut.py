4c8aea0070241850d43deab1fdecc032358989dd
grogsy <a.gray.cloud@gmail.com>
2018-07-04 18:30:01 -0300

    Add files via upload

--- unix_replica/unix_things/cut.py
+++ unix_replica/unix_things/cut.py
@@ -4,11 +4,12 @@ import sys
 def parse_args():
     parser = argparse.ArgumentParser(description="Unix cut")
     parser.add_argument('file', type=str)
-    parser.add_argument('-d', type='str', help="Delimiter to indicate fields")
-    parser.add_argument('-f', type='str', help="Dash(-) separated values for slice grabbing")
-    parser.add_argument('-c', type='str', help="Comma separated values for index grabbing")
+    parser.add_argument('-d', type=str, help="Delimiter to indicate fields")
+    parser.add_argument('-f', type=str, help="Dash(-) separated values for slice grabbing")
+    parser.add_argument('-c', type=str, help="Comma separated values for index grabbing")
     parser.add_argument('-s', action='store_true',
                         help='if -d do not print lines without delimiter')
+    return parser.parse_args()
 
 
 def get_slice(fields):
@@ -44,13 +45,30 @@ def main(args):
         build = []
         for line in txt:
             build.append(delimiter.join(line.split(delimiter)[indices]))
-        sys.stdout.write('\n'.join(build))
+        sys.stdout.write(''.join(build))
+
     elif chars:
-        indices = chars.split(',')
         build = []
-        for line in txt:
-            build.append(''.join(line[int(index)] for index in indices))
-        sys.stdout.write('\n'.join(build))
+
+        if '-' in chars:
+            indices = get_slice(chars)
+            for line in txt:
+                build.append(line[indices])
+            sys.stdout.write('\n'.join(build))
+
+        elif ',' in chars:
+            indices = chars.split(',')
+            if '' in indices:
+                exit("Can't leave stray comma(,) in -c")
+            for line in txt:
+                build.append(''.join(line[int(index)] for index in indices))
+            sys.stdout.write('\n'.join(build))
+
+        else:
+            # Only one index given
+            index = int(chars)
+            sys.stdout.write('\n'.join(line[index] for line in txt))
+
 
 if __name__=='__main__':
     main(vars(parse_args()))
