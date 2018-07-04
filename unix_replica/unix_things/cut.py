5700b51ff9145666fb9cbce95ecfc19a5d98d9f9
grogsy <a.gray.cloud@gmail.com>
2018-07-04 04:42:06 -0300

    Add files via upload

---
+++ unix_replica/unix_things/cut.py
@@ -0,0 +1,52 @@
+import argparse
+import sys
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Unix cut")
+    parser.add_argument('file', type=str)
+    parser.add_argument('-d', type='str', help="Delimiter to indicate fields")
+    parser.add_argument('-f', type='str', help="Dash(-) separated values for slice grabbing")
+    parser.add_argument('-c', type='str', help="Comma separated values for index grabbing")
+
+
+def get_slice(fields):
+    lower, upper = fields.split('-')
+
+    try:
+        lower = int(lower) - 1
+    except ValueError:
+        lower = None
+    try:
+        upper = int(upper)
+    except ValueError:
+        upper = None
+
+    return slice(lower, upper)
+
+
+def main(args):
+    txt = [line for line in open(args['file'], 'r').readlines()]
+    delimiter = args['d']
+    fields = args['f']
+    chars = args['c']
+
+    if delimiter and not fields:
+        exit("Cannot operate with delimiter and no fields")
+    if fields and chars:
+        exit("Can only choose one of these options: -f/-c")
+
+    if fields:
+        indices = get_slice(fields)
+        build = []
+        for line in txt:
+            build.append(delimiter.join(line.split(delimiter)[indices]))
+        sys.stdout.write('\n'.join(build))
+    elif chars:
+        indices = chars.split(',')
+        build = []
+        for line in txt:
+            build.append(''.join(line[int(index)] for index in indices))
+        sys.stdout.write('\n'.join(build))
+
+if __name__=='__main__':
+    main(vars(parse_args()))
