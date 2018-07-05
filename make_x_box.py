5452226ac5de6ca4bc6d6ce318eed682208746c1
grogsy <a.gray.cloud@gmail.com>
2018-07-05 04:37:57 -0300

    Create make_x_box.py

---
+++ make_x_box.py
@@ -0,0 +1,14 @@
+# I hate this
+# Some interview question posted on r/programmerhumor
+
+def make_x_box(width, height):
+    for y in range(height):
+        build = 'x'
+        for x in range(width):
+            if y == 0 or y == height-1 or x==y-1 or y+x == width:
+                build += 'x'
+            else:
+                build += ' '
+        build += 'x'
+        print(build)
+make_x_box(10,10)
