6b6ecfa1711c71dd9d2e8e51122aa585e4ef9c02
grogsy <a.gray.cloud@gmail.com>
2018-07-06 04:42:49 -0300

    Create collections_ordereddict.py

---
+++ hacker_rank/collections_ordereddict.py
@@ -0,0 +1,23 @@
+from collections import OrderedDict
+
+sales = OrderedDict()
+
+num = int(input())
+
+for _ in range(num):
+    entries = input().split()
+    item_name = []
+    for entry in entries:
+        try:
+            net_price = int(entry)
+        except ValueError:
+            item_name.append(entry)
+    item_name = ' '.join(item_name)
+    
+    if item_name in sales:
+        sales[item_name] += int(net_price)
+    else:
+        sales[item_name] = net_price
+
+for item, price in sales.items():
+    print(item, price)
