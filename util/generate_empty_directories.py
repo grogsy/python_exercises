896e4e215d61a5d0d4f3e5a2c8fa947d3d9f4049
Brian <a.gray.cloud@gmail.com>
2018-07-13 17:30:20 -0300

    used to test rm.py

--- util/generate_empty_directories.py
+++ util/generate_empty_directories.py
@@ -1,16 +1,21 @@
+'''
+Used this to test rm.py
+Go to any throw-away directory you choose and and run generate_random_directories.py
+'''
+import argparse
 import os
 import random
 
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-def make_dir(make_files=False):
+def make_dir(make_files=False, num_of_files=None):
     for i in range(5):
         dir_name = ''.join(random.choice(alphabet) for _ in range(5))
         os.mkdir(dir_name)
         print("%s succesfully created" % dir_name)
         if make_files:
-            mkf(dir_name)
+            mkf(dir_name, num_of_files)
 
 
 def mkf(directory, amt=2):
@@ -22,3 +27,15 @@ def mkf(directory, amt=2):
         print("%s successfully created" % full_name)
 
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-mkf', action='store_true')
+    parser.add_argument('-n', metavar='--number-of-files', type=int, default=2,
+                        help='with -mkf specify how many throwaway files to put in each folder')
+    args = vars(parser.parse_args())
+
+    if args['mkf']:
+        amount = args['n'] or 2
+        make_dir(make_files=True, num_of_files=amount)
+    else:
+        make_dir()
