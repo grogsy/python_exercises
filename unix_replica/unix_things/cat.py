770a966e1416390194a50dbcdb1e19dc69fe823a
Brian <a.gray.cloud@gmail.com>
2018-07-17 00:24:13 -0300

    put things into functions

--- unix_replica/unix_things/cat.py
+++ unix_replica/unix_things/cat.py
@@ -17,52 +17,60 @@ def parse_args():
     return parser.parse_args()
 
 
+def run_as_echo(args):
+    c = 1
+    while True:
+        txt = input()
+        if args['E']:
+            txt += '$'
+        if args['T']:
+            txt = txt.replace('\t', '^I')
+        if args['n']:
+            txt = LINE_FMT.format(c, txt, w=5)
+            c+=1
+        if args['b']:
+            txt = txt.strip('\n').strip('\t')
+            if txt:
+                txt = LINE_FMT.format(c, txt, w=5)
+                c += 1
+            else:
+                pass
+        sys.stdout.write(txt+'\n')
+
+
+def run_with_files(args):
+    txt = [line.strip('\n') for f in args['files'] for line in open(f, 'r').readlines()]
+    if args['s']:
+        txt = [line for line in txt if line]
+    if args['E']:
+        txt = [line+'$' for line in txt]
+    if args['T']:
+        txt = [line.replace('\t', '^I') for line in txt]
+
+    if args['n']:
+        new_txt = list(enumerate(txt, start=1))
+        num_width = len(str(len(txt)))
+        sys.stdout.write('\n'.join(LINE_FMT.format(num, line, w=num_width) for num, line in new_txt))
+    elif args['b']:
+        c = 1
+        new_txt = []
+        width = len(str(len(txt)))
+        for line in txt:
+            if line:
+                new_txt.append(LINE_FMT.format(c, line, w=width))
+                c += 1
+            else:
+                new_txt.append(line)
+        sys.stdout.write('\n'.join(new_txt))
+    else:
+        sys.stdout.write('\n'.join(line for line in txt))
+
+
 def main(args):
     if not args['files']:
-        c = 1
-        while True:
-            txt = input()
-            if args['E']:
-                txt += '$'
-            if args['T']:
-                txt = txt.replace('\t', '^I')
-            if args['n']:
-                txt = LINE_FMT.format(c, txt, w=5)
-                c+=1
-            if args['b']:
-                txt = txt.strip('\n').strip('\t')
-                if txt:
-                    txt = LINE_FMT.format(c, txt, w=5)
-                    c += 1
-                else:
-                    pass
-            sys.stdout.write(txt+'\n')
+        run_as_echo(args)
     else:
-        txt = [line.strip('\n') for f in args['files'] for line in open(f, 'r').readlines()]
-        if args['s']:
-            txt = [line for line in txt if line]
-        if args['E']:
-            txt = [line+'$' for line in txt]
-        if args['T']:
-            txt = [line.replace('\t', '^I') for line in txt]
-
-        if args['n']:
-            new_txt = list(enumerate(txt, start=1))
-            num_width = len(str(len(txt)))
-            sys.stdout.write('\n'.join(LINE_FMT.format(num, line, w=num_width) for num, line in new_txt))
-        elif args['b']:
-            c = 1
-            new_txt = []
-            width = len(str(len(txt)))
-            for line in txt:
-                if line:
-                    new_txt.append(LINE_FMT.format(c, line, w=width))
-                    c += 1
-                else:
-                    new_txt.append(line)
-            sys.stdout.write('\n'.join(new_txt))
-        else:
-            sys.stdout.write('\n'.join(line for line in txt))
+        run_with_files(args)
 
 
 if __name__ == '__main__':
