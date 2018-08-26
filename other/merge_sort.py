7898b9e2ef17bea485f5a51e63428e7195aabdbc
Brian <a.gray.cloud@gmail.com>
2018-08-26 17:16:27 -0300

    upload

---
+++ other/merge_sort.py
@@ -0,0 +1,21 @@
+def sort(arr):
+    if len(arr) == 0 or len(arr) == 1:
+        return arr
+    elif len(arr) == 2:
+        if arr[0] > arr[1]:
+            arr[0], arr[1] = arr[1], arr[0]
+        return arr
+    else:
+        pivot = arr[0]
+        lower = []
+        higher = []
+        equal = []
+        for item in arr:
+            if item < pivot:
+                lower.append(item)
+            elif item > pivot:
+                higher.append(item)
+            else:
+                equal.append(item)
+
+    return sort(lower)+sort(equal)+sort(higher)
