d88bb0f5e32bbb122a93a6158939751bcc53f70b
Brian <a.gray.cloud@gmail.com>
2018-08-29 00:18:35 -0300

    fixed things

--- datastructures/lmpthw/sorts/bubblesort.py
+++ datastructures/lmpthw/sorts/bubblesort.py
@@ -1,3 +1,4 @@
+'''Bubblesort'''
 def sort(arr):
     if arr.count == 1 or arr.count == 0:
         return
@@ -5,9 +6,11 @@ def sort(arr):
     while swapped:
         swapped = False
         for node in arr:
-            if node is arr.tail:
-                break
-            nxt = node.next
-            if node.value > nxt.value:
-                node.value, nxt.value = nxt.value, node.value
-                swapped = True
+            prev = node.prev
+            try:
+                if prev.value > node.value:
+                    node.value, prev.value = prev.value, node.value
+                    swapped = True
+            except (TypeError, AttributeError):
+                # The case of the head as it does not have a previous value to compare
+                pass
--- datastructures/lmpthw/test_sorting.py
+++ datastructures/lmpthw/test_sorting.py
@@ -1,25 +1,31 @@
-from structures.double_linked_list import DoubleLinkedList as Array
 from random import choice
+from structures.double_linked_list import DoubleLinkedList as Array
 
 
-def test_bubblesort():
-    from sorts.bubblesort import sort
+def test_sort(sort_func):
     for _ in range(10):
         length = choice(range(10, 100))
         arr = Array()
         for _ in range(length):
             arr.push(choice(range(10000)))
-        sort(arr)
+        sort_func(arr)
         for node in arr:
-            if node is arr.tail:
-                break
-
-            nxt = node.next
-            if node is arr.head:
-                assert node.value <= nxt.value
-            else:
-                prev = node.prev
+            nxt  = node.next
+            prev = node.prev
+            try:
                 assert prev.value <= node.value <= nxt.value
+            except (TypeError, AttributeError):
+                try:
+                    # Is it the head?
+                    assert node.value <= nxt.value
+                except (TypeError, AttributeError):
+                    # Must be the tail
+                    assert prev.value <= node.value
+
+
+
+def test_bubblesort():
+    from sorts.bubblesort import sort
+    test_sort(sort)
 
 test_bubblesort()
-
