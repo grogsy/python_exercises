40158aa899638dc971baa9db795b9d5a5e29f5dd
grogsy <a.gray.cloud@gmail.com>
2018-11-20 21:46:53 -0400

    Create urlify.py

---
+++ interview_question_examples/ctci/urlify.py
@@ -0,0 +1,24 @@
+# with builtin func
+def urlfiy(string):
+  return string.replace(' ', '%20')
+  
+# without
+def urlify2(string):
+  res = []
+  for c in string:
+    if c == ' ':
+      res.append('%20')
+    else:
+      res.append(c)
+  
+  return ''.join(res)
+  
+def urlify3(string):
+  res = ''
+  for c in string:
+  if c == ' ':
+    res += '%20'
+  else:
+    res += c
+    
+  return res
