9a1d84d123e1675f8e9ebbbf3435f369a90dd64f
grogsy <a.gray.cloud@gmail.com>
2018-12-29 15:19:08 -0400

    Update is_permutation.py

--- interview_question_examples/ctci/arrays_and_strings/is_permutation.py
+++ interview_question_examples/ctci/arrays_and_strings/is_permutation.py
@@ -9,3 +9,7 @@ class CounterPermutationChecker(Counter):
     def __eq__(self, other):
         return all(self[counter] == other[other_counter]
                    for counter, other_counter in zip(sorted(self.keys()), sorted(other.keys())))
+    
+# woops there's actually a much simple approach
+def is_permutation(word1, word2):
+    return sorted(word1) == sorted(word2)
