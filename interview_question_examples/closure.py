d8ab34bdebeefdf540e8756e82072b74d5706d76
grogsy <a.gray.cloud@gmail.com>
2019-06-19 19:55:16 -0300

    Create closure.py
    closure example in python

---
+++ interview_question_examples/closure.py
@@ -0,0 +1,23 @@
+def secret_people(people_list, secret):
+    def mask(guest):
+        def validate(secret_code):
+            return guest if secret_code == secret else "invalid code"
+        return validate
+        
+    return [mask(guest) for guest in people_list]
+    
+    
+ppl = 'me you them'.split(' ')
+
+secret_list = secret(ppl, 512)
+
+# every item is a function object
+print(secret_list)
+
+# returns "invalid code"
+for s in secret_list:
+    print(s(513))
+    
+# returns items from list ppl
+for s in secret_list:
+    print(s(512))
