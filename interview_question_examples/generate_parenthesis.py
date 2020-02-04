ee89cb6f5c338cce71ba66dd0d36221fd53c38e2
Brian <a.gray.cloud@gmail.com>
2020-02-04 15:46:45 -0500

    Create generate_parenthesis.py

---
+++ interview_question_examples/generate_parenthesis.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/generate-parentheses/submissions/
+
+def generate_parenthesis(n):
+    braces = []
+    def _generate_parenthesis(l_count, r_count, brace):
+        def _generate_para(l_count, r_count, brace):
+            if l_count < 0 or r_count < 0 or l_count > r_count:
+                return 
+            if l_count == 0 and r_count == 0:
+                braces.append(brace)
+                return
+            _generate_parenthesis(l_count - 1, r_count, brace + '(')
+            _generate_parenthesis(l_count, r_count - 1, brace + ')')
+
+    _generate_parenthesis(n, n, '')
+        
+    return braces
