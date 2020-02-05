d63d58f8e7447f52f305e50dbb2a118018891803
Brian <a.gray.cloud@gmail.com>
2020-02-04 20:07:28 -0500

    Update generate_parenthesis.py

--- interview_question_examples/generate_parenthesis.py
+++ interview_question_examples/generate_parenthesis.py
@@ -2,15 +2,15 @@
 
 def generate_parenthesis(n):
     braces = []
+
     def _generate_parenthesis(l_count, r_count, brace):
-        def _generate_parenthesis(l_count, r_count, brace):
-            if l_count < 0 or r_count < 0 or l_count > r_count:
-                return 
-            if l_count == 0 and r_count == 0:
-                braces.append(brace)
-                return
-            _generate_parenthesis(l_count - 1, r_count, brace + '(')
-            _generate_parenthesis(l_count, r_count - 1, brace + ')')
+        if l_count < 0 or r_count < 0 or l_count > r_count:
+            return 
+        if l_count == 0 and r_count == 0:
+            braces.append(brace)
+            return
+        _generate_parenthesis(l_count - 1, r_count, brace + '(')
+        _generate_parenthesis(l_count, r_count - 1, brace + ')')
 
     _generate_parenthesis(n, n, '')
         
