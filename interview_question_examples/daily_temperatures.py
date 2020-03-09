ee15db2dd4c6c3b1dff71c8d125f09f0483b4f85
Brian <a.gray.cloud@gmail.com>
2020-03-09 00:58:12 -0400

    Update daily_temperatures.py

--- interview_question_examples/daily_temperatures.py
+++ interview_question_examples/daily_temperatures.py
@@ -1,18 +1,18 @@
 # https://leetcode.com/problems/daily-temperatures/
 
-def daily_temperatures(T):
-    t = T
+def daily_temperatures(t):
     output = [0] * len(t)
-
     pending = []
 
     for i in range(len(t) - 1):
-        if pending and t[i + 1] > t[pending[-1]]:
+        while pending and t[i + 1] > t[pending[-1]]:
             index = pending.pop()
             output[index] = (i + 1) - index
-            while pending and t[pending[-1]] < t[i + 1]:
-                index = pending.pop()
-                output[index] = (i + 1) - index
+        if t[i] < t[i + 1]:
+            output[i] += 1
+        else:
+            pending.append(i)
+
         if t[i] < t[i + 1]:
             output[i] += 1
         else:
